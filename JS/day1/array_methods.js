bda596e9ae585e78ec46700b060278d68b44ef46
AkaCoder007 <78375228+AkaCoder007@users.noreply.github.com>
2021-06-21 10:55:46 +0530

    removed unneeded folders
    -removed js files
    -removed  cn ninjas.css

--- JS/day1/array_methods.js
+++
@@ -1,5 +0,0 @@
-const arr = [2, 3, 4, 32, 4];
-arr.push(7);
-console.log(arr);
-arr.pop();
-console.log(arr);
--- JS/day1/this.js
+++
@@ -1,9 +0,0 @@
-const thisExample = {
-    name: "ANil",
-    thisfunc: function() {
-        console.log(this); //refers to this object
-    },
-
-    thisfunc: () => (console.log(this)) //refers to empty object
-};
-thisExample.thisfunc(); ///refers to current object
